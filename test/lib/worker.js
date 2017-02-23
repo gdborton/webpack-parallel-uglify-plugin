ec67b2ecba65a954d4e15c88f4c176ba51a3487a
Gary Borton <gary.borton@airbnb.com>
2017-02-23 02:22:04 -0800

    Change cacheKey generation from JSON.stringify to a regular string.
    Using JSON.stringify on an object containing an asset's source was pretty slow.
    Concatenating the source with the useSourceMaps variable still produces a
    unique string, and is many times faster.

--- test/lib/worker.js
+++ test/lib/worker.js
@@ -65,7 +65,7 @@ test('messageHandler should handle minify messages, minifying the provided file.
 
   t.true(stubbedUpdate.calledWith(tmpFileName, JSON.stringify(minifiedContent)));
   const cacheKey = cache.createCacheKey(
-    JSON.stringify({ source: codeSource, useSourceMaps: false }),
+    codeSource + false,
     options
   );
   t.true(stubbedSend.calledWith({
