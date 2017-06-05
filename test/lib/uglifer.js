9a4d7fa4830ef171e5d7b9a1e398a2987fab47c9
Gary Borton <gary.borton@airbnb.com>
2017-06-05 13:20:37 -0700

    [Config] Add support for test/include/exclude.
    This commit adds support for the test/include/exclude options that are fairly
    standard across Webpack plugins. This uses a utility method from Webpack to
    ensure correctness.

--- test/lib/uglifer.js
+++ test/lib/uglifer.js
@@ -8,16 +8,28 @@ const {
 } = require('../../lib/uglifier');
 
 const filename = 'somefile.js';
-const fakeCompilationObject = {
-  assets: {
-    [filename]: {
-      source() {
-        return 'function    name()   {   }';
+const testedFilename = 'testedFilename.js';
+
+const unminifedSource = 'function    name()   {   }';
+const minifiedSource = 'function name(){}';
+
+function createFakeCompilationObject() {
+  return {
+    assets: {
+      [filename]: {
+        source() {
+          return unminifedSource;
+        },
+      },
+      [testedFilename]: {
+        source() {
+          return unminifedSource;
+        },
       },
     },
-  },
-  options: {},
-};
+    options: {},
+  };
+}
 
 test('workerCount should be cpus - 1 if assetCount is >= cpus', t => {
   const cpuStub = sinon.stub(os, 'cpus', () => ({ length: 8 }));
@@ -34,9 +46,49 @@ test('workerCount should be assetCount if assetCount is < cpus', t => {
 });
 
 test.cb('processAssets minifies each of the assets in the compilation object', (t) => {
+  const fakeCompilationObject = createFakeCompilationObject();
   processAssets(fakeCompilationObject, {}).then(() => {
-    const minifiedSource = fakeCompilationObject.assets[filename].source();
-    t.is(minifiedSource, 'function name(){}');
+    const minifiedResult = fakeCompilationObject.assets[filename].source();
+    t.is(minifiedResult, minifiedSource);
+    t.end();
+  });
+});
+
+test.cb('processAssets respects test option', (t) => {
+  const fakeCompilationObject = createFakeCompilationObject();
+  processAssets(fakeCompilationObject, {
+    test: /tested/,
+  }).then(() => {
+    const unmatchedResult = fakeCompilationObject.assets[filename].source();
+    const matchedResult = fakeCompilationObject.assets[testedFilename].source();
+    t.is(unmatchedResult, unminifedSource);
+    t.is(matchedResult, minifiedSource);
+    t.end();
+  });
+});
+
+test.cb('processAssets respects include option', (t) => {
+  const fakeCompilationObject = createFakeCompilationObject();
+  processAssets(fakeCompilationObject, {
+    include: [/tested/],
+  }).then(() => {
+    const unmatchedResult = fakeCompilationObject.assets[filename].source();
+    const matchedResult = fakeCompilationObject.assets[testedFilename].source();
+    t.is(unmatchedResult, unminifedSource);
+    t.is(matchedResult, minifiedSource);
+    t.end();
+  });
+});
+
+test.cb('processAssets respects exclude option', (t) => {
+  const fakeCompilationObject = createFakeCompilationObject();
+  processAssets(fakeCompilationObject, {
+    exclude: [/tested/],
+  }).then(() => {
+    const unmatchedResult = fakeCompilationObject.assets[filename].source();
+    const matchedResult = fakeCompilationObject.assets[testedFilename].source();
+    t.is(unmatchedResult, minifiedSource);
+    t.is(matchedResult, unminifedSource);
     t.end();
   });
 });
