c742de92aa5dbb9242ae1e6a976d3d939c8811c6
Gary <gdborton@gmail.com>
2020-12-05 14:50:24 -0800

    Update webpack plugin api to v5

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function sourceMapError(lib) {
   return `You should not pass options.${lib}.sourceMap, did you mean options.sourceMap?`;
 }
 
-function FasterUglifyPlugin(options) {
+function ParallelUglifyPlugin(options) {
   if (options.uglifyJS && options.terser) {
     throw new TypeError('You cannot use both uglifyJS and terser for the same plugin.');
   }
@@ -23,18 +23,21 @@ function FasterUglifyPlugin(options) {
   }
 }
 
-FasterUglifyPlugin.prototype.apply = function apply(compiler) {
-  compiler.plugin('compilation', (compilation) => {
-    compilation.plugin('optimize-chunk-assets', (chunks, callback) => {
+ParallelUglifyPlugin.prototype.apply = function apply(compiler) {
+  compiler.hooks.compilation.tap('ParallelUglifyPlugin', (compilation) => {
+    compilation.hooks.processAssets.tapAsync({
+      name: 'ParallelUglifyPlugin',
+      stage: compilation.PROCESS_ASSETS_STAGE_OPTIMIZE_SIZE,
+    }, (chunks, callback) => {
       uglifier.processAssets(compilation, this.options).then(() => {
         callback();
       });
     });
   });
 
-  compiler.plugin('done', () => {
+  compiler.hooks.done.tap('ParallelUglifyPlugin', () => {
     uglifier.pruneCache(this.options);
   });
 };
 
-module.exports = FasterUglifyPlugin;
+module.exports = ParallelUglifyPlugin;
